769ec216f06d4c8e0f5329d632dc2dcbb48b8b17
Mr. M <31281224+spherolearn@users.noreply.github.com>
2017-08-23 10:08:38 -0400

    just a test
    just a test - setting up github

---
+++ test.js
@@ -0,0 +1,26 @@
+async function startProgram() {
+        // Write code here
+
+        speak('throw the ball in the air to hear that scrambled word, collide with the ball to hear the solution');
+        await delay(8);
+        var word = "JAMES";
+        var hint = 'A first name for a man';
+        var ans = word;
+        var wordLength = word.length;
+        var scrambled = "";
+
+        for (var i = 0; i < wordLength; i++) {
+                var charIndex = Math.floor(Math.random() * word.length);
+                scrambled += word.charAt(charIndex) + ' , ';
+                word = word.substr(0, charIndex) + word.substr(charIndex + 1);
+        }
+        speak('The scrambled word is:' + scrambled);
+        await delay(wordLength);
+        speak('The answer is:' + ans);
+        await delay(wordLength);
+
+
+        //varFirstLetter =
+        speak('Here is a hint, The word starts with the letter: ' + ans.substring(0, 1) + hint)
+        // Put the other hand here
+}
