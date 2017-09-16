c331e7691e6ec5c949ef524e0c85f9ac9249aa44
spherolearn <spherolearn@gmail.com>
2017-09-16 13:39:43 -0400

    This was PULLED DOWN, and them pushed back up.

--- fromgithubdotcom.js
+++ fromgithubdotcom.js
@@ -1,1 +1,4 @@
-Test of a file created on github.com, and hopefully pushed down.
+//Test of a file created on github.com, and hopefully pushed down.
+
+//  that worked, I pulled it down... in XSFC in atom
+// from USB drive
